367ba52d0e87d7ca9a5ac37aad7509e0ff6d3295
Will Chapman <will@lightfeather.io>
2022-12-28 20:32:07 -0500

    Updated AwsUtils

--- vars/AwsUtils.groovy
+++ vars/AwsUtils.groovy
@@ -1,3 +1,14 @@
+def _curl(String path) {
+    return sh(script: "curl -s http://169.254.169.254/latest/${path}", returnStdout: true).trim()
+}
+def getAvailabilityZone() {
+    return _curl("meta-data/placement/availability-zone")
+}
+
 def getRegion() {
-    return sh(script: "curl http://169.254.169.254/", returnStdout: true)
+    return _curl("meta-data/placement/region")
+}
+
+def getIdentityDocument() {
+    return _curl("dynamic/instance-identity/document")
 }
