e74c3de2700cc83f6c18f99734f1b0cb0ea8345c
Will Chapman <will@lightfeather.io>
2022-12-28 19:48:11 -0500

    Adding npm authentication in nodejs.groovy

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,7 +1,14 @@
 def call(Map args, Closure body) {
     withCredentials([gitUsernamePassword(credentialsId: args.githubCredsId, gitToolName: 'git-tool')]) {
         withDockerContainer(image: args.nodeImage, args: "-e HOME=${args.nodeHome}") {
-            body()
+            try {
+                sh "echo registry=https://npm.pkg.github.com >> .npmrc"
+                sh "echo //npm.pkg.github.com/:_authToken=${env.GIT_PASSWORD} >> .npmrc"
+
+                body()
+            } finally {
+                sh 'rm ./.npmrc'
+            }
         }
     }
 }
