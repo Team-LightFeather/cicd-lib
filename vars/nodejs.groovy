7213afae04ea4dfe5f4f6b8dfeeba4f4347e7160
Will Chapman <will@lightfeather.io>
2022-12-28 20:06:22 -0500

    Made githubCredsId optional in nodejs.groovy

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,14 +1,33 @@
+/**
+ * Sets up a nodejs environment to run commands in (via docker)
+ *
+ * Supported arguments
+ *  githubCredsId: if set, this will setup a temporary .npmrc file for authenticating to the github npm package repo
+ *  nodeImage: What docker image to use for the node environment
+ *  nodeHome: What to set the HOME environment variable to in the docker container
+ *
+ * @param args
+ * @param body
+ * @return
+ */
 def call(Map args, Closure body) {
-    withCredentials([gitUsernamePassword(credentialsId: args.githubCredsId, gitToolName: 'git-tool')]) {
-        withDockerContainer(image: args.nodeImage, args: "-e HOME=${args.nodeHome}") {
-            try {
-                sh "echo registry=https://npm.pkg.github.com >> .npmrc"
-                sh "echo //npm.pkg.github.com/:_authToken=${env.GIT_PASSWORD} >> .npmrc"
+    if(args.containsKey("githubCredsId")) {
+        withCredentials([gitUsernamePassword(credentialsId: args.githubCredsId, gitToolName: 'git-tool')]) {
+            withDockerContainer(image: args.nodeImage, args: "-e HOME=${args.nodeHome}") {
+                try {
+                    sh "echo registry=https://npm.pkg.github.com >> .npmrc"
+                    sh "echo //npm.pkg.github.com/:_authToken=${env.GIT_PASSWORD} >> .npmrc"
 
-                body()
-            } finally {
-                sh 'rm ./.npmrc'
+                    body()
+                } finally {
+                    sh 'rm ./.npmrc'
+                }
             }
         }
+    } else {
+        withDockerContainer(image: args.nodeImage, args: "-e HOME=${args.nodeHome}") {
+            body()
+        }
     }
+
 }
