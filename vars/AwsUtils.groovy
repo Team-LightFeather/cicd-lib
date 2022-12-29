578d5ca7fda61ed1e20ebe703b8da0ed8dc74b67
Will Chapman <will@lightfeather.io>
2022-12-28 20:40:23 -0500

    Updated AwsUtils with getting account number

--- vars/AwsUtils.groovy
+++ vars/AwsUtils.groovy
@@ -1,6 +1,11 @@
 def _curl(String path) {
     return sh(script: "curl -s http://169.254.169.254/latest/${path}", returnStdout: true).trim()
 }
+
+def _curljq(String path, String jqFilter) {
+    return sh(script: "curl -s http://169.254.169.254/latest/${path} | jq ${jqFilter}", returnStdout: true).trim()
+}
+
 def getAvailabilityZone() {
     return _curl("meta-data/placement/availability-zone")
 }
@@ -11,4 +16,8 @@ def getRegion() {
 
 def getIdentityDocument() {
     return _curl("dynamic/instance-identity/document")
+}
+
+def getAccountNumber() {
+    return _curljq("dynamic/instance-identity/document", ".AccountId")
 }
