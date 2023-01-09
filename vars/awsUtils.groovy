7af84ae15631b115e9d2c31e2a7c1a426a69436c
mdere <michael.dere@lightfeather.io>
2023-01-09 02:03:02 +0000

    updatedg

--- vars/awsUtils.groovy
+++ vars/awsUtils.groovy
@@ -1,34 +1,34 @@
-static def _curl(String path) {
+def _curl(String path) {
     return sh(script: "curl -s http://169.254.169.254/latest/${path}", returnStdout: true).trim()
 }
 
-static def _curljq(String path, String jqFilter) {
+def _curljq(String path, String jqFilter) {
     return sh(script: "curl -s http://169.254.169.254/latest/${path} | jq ${jqFilter}", returnStdout: true).trim()
 }
 
-static def  getAvailabilityZone() {
+def  getAvailabilityZone() {
     return _curl("meta-data/placement/availability-zone")
 }
 
-static def getRegion() {
+def getRegion() {
     return _curl("meta-data/placement/region")
 }
 
-static def getIdentityDocument() {
+def getIdentityDocument() {
     return _curl("dynamic/instance-identity/document")
 }
 
-static def getAccountNumber() {
+def getAccountNumber() {
     return _curljq("dynamic/instance-identity/document", ".AccountId")
 }
 
-static def getEcrImageUrl(String containerName, String tag) {
+def getEcrImageUrl(String containerName, String tag) {
     if(tag == null)
         return "${awsEcrEndpoint()}/${containerName}";
     else
         return "${awsEcrEndpoint()}/${containerName}:${tag}";
 }
 
-static def awsEcrEndpoint() {
+def awsEcrEndpoint() {
     return "${getAccountNumber()}.dkr.ecr.${getRegion()}.amazonaws.com"
 }
