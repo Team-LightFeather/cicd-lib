3e5bed75e1cf96cccc992a5db1632a128c6afbff
mdere <michael.dere@lightfeather.io>
2023-01-21 10:41:22 -0500

    added baseline files

--- vars/orchestration.groovy
+++ vars/orchestration.groovy
@@ -5,10 +5,25 @@ It will ingest a build parameter that will configure the workflow
 def call(buildConfig) {
     // Setup
     setup(buildConfig)
+    if (isApplicationPipeline(buildConfig)) {
+        applicationPipeline(buildConfig)
+    }
+    if (isInfraPipeline(buildConfig)) {
+        terraformPipeline(buildConfig)
+    }
+}
+
+def applicationPipeline(buildConfig) {
     // Build
     build(buildConfig)
     // Scan
     scan(buildConfig)
     // Deploy
     deploy(buildConfig)
+}
+
+def terraformPipeline(buildConfig) {
+    terraformInit(buildConfig)
+    terraformPlan(buildConfig)
+    terraformApply(buildConfig)
 }
--- vars/setup.groovy
+++ vars/setup.groovy
@@ -8,19 +8,24 @@ def call(buildConfig) {
         sh "echo \'Beginning Setup Process...\'"
         def system = buildConfig.system.toLowerCase()
         env.AWS_DEFAULT_REGION = awsUtils.getRegion()
-        if (system == "java") {
+        if (system == "springboot") {
             env.OUTPUT_DIR = "build/libs"
             env.TEST_REPORT_DIR = ""
         }
-        if (system == "nodejs") { // Note we need to define this further, not sure if system is correct, maybe framework? springboot vs reactjs?
+        if (system == "reactjs") { // Note we need to define this further, not sure if system is correct, maybe framework? springboot vs reactjs?
             env.OUTPUT_DIR = "build"
             env.TEST_REPORT_DIR = ""
+        }
+        if (system == "terraform") {
+            
         }
         /*
         This is to import docker and helm and other resources to be used in the pipeline
         */
-        dir("cicd-lib") {
-            git url: 'https://github.com/Team-LightFeather/cicd-lib.git', branch: 'feature/orchestration', credentialsId: 'github'
+        if (system == "springboot") {
+            dir("cicd-lib") {
+                git url: 'https://github.com/Team-LightFeather/cicd-lib.git', branch: 'feature/orchestration', credentialsId: 'github'
+            }
         }
     }
 }
