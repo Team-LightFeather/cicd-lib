dcb9ba20e95b189ccc0c4c264fef1c6ffd85625c
mdere <michael.dere@lightfeather.io>
2023-01-22 15:30:10 -0500

    updated

--- vars/pushToECR.groovy
+++ vars/pushToECR.groovy
@@ -1,11 +1,30 @@
 def call(buildConfig) {
+    ecrContainer(buildConfig) {
+        d -> run(buldConfig)
+    }
+
+}
+
+def ecrContainer(buildConfig, closure) {
+    sh "aws ecr get-login-password --region ${env.AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${awsUtils.awsEcrEndpoint()};"
+    closure()
+}
+
+def run(buildConfig) {
     def imageName = buildConfig.container.imageName
     def buildImageTag = awsUtils.getEcrImageUrl(imageName, env.GIT_COMMIT)
     def pushImageTag = awsUtils.getEcrImageUrl(imageName, "latest")
+    createIfNotExist(imageName)
     sh """
         aws ecr get-login-password --region ${env.AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${awsUtils.awsEcrEndpoint()}
         docker push ${buildImageTag}
         docker tag ${buildImageTag} ${pushImageTag}
         docker push ${pushImageTag}
+    """
+}
+
+def createIfNotExist(String imageName) {
+    sh """
+        aws ecr create-repository --repository-name ${imageName} || true;
     """
 }
